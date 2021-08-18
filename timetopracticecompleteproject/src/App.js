6142ea1ae2af67ac43085cf162725b36a40fe1c1
Guilherme Adriano Schafer <guilherme.schafer@ambevtech.com.br>
2021-08-18 19:49:30 -0300

    Time to Practicec: A Complete Pratice Project

---
+++ timetopracticecompleteproject/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./App.css";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          id: Math.random().toString(),
+          name: uName,
+          age: uAge,
+        },
+      ];
+    });
+  };
+
+  return (
+    <div className="App">
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
